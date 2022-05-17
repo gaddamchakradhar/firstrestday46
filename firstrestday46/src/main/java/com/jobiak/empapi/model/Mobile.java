de3b78d0139c268798550c9c5a1b2119dfe63c8f
chakradhar <chakradharreddy9642@gmail.com.com>
2022-05-17 10:31:23 +0530

    pushing the project to git

---
+++ firstrestday46/src/main/java/com/jobiak/empapi/model/Mobile.java
@@ -0,0 +1,59 @@
+package com.jobiak.empapi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Mobile {
+	@Id
+	@GeneratedValue
+	private Long mid;
+	private String brand;
+	private String model;
+	private double price;
+	
+	public Mobile() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public long getMid() {
+		return mid;
+	}
+
+	public void setMid(long mid) {
+		this.mid = mid;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Mobile [mid=" + mid + ", brand=" + brand + ", model=" + model + ", price=" + price + "]";
+	}
+
+
+
+}
